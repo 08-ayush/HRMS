c922e10d7d02df91508fa23c654debf768e1bd0d
aayush <aayushchauhan008@gmail.com>
2026-02-24 21:50:02 +0530

    "edit-config"

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,14 +5,19 @@ load_dotenv()
 
 
 class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5433")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
     DB_NAME: str = os.getenv("DB_NAME", "project")
     DB_USER: str = os.getenv("DB_USER", "postgres")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "newpassword")
 
     @property
-    def DATABASE_URL(self) -> str:
+    def database_url(self) -> str:
+        if self.DATABASE_URL:
+            return self.DATABASE_URL  # Use Render DB if available
+
         return (
             f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
